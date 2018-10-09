446a488a3e58d88bc28ede3cb6d64eaab2fb4d25
yugen-yeap <36917057+yugen-yeap@users.noreply.github.com>
2018-10-09 17:24:25 +1000

    Board layout and and some menu parts working

--- minesweeper/Minesweeper.c
+++ minesweeper/Minesweeper.c
@@ -11,6 +11,7 @@
 
 char board[NUM_TILES_X][NUM_TILES_Y], *mine = "*";
 int mine_positions[NUM_MINES][2];
+int x_input[1], y_input[1];
 
 typedef struct{
   int adjacent_mines;
@@ -18,10 +19,14 @@ typedef struct{
   bool is_mine;
 } Tile;
 
+
+
 typedef struct{
-  bool won;
   Tile tiles[NUM_TILES_X][NUM_TILES_Y];
+  bool running;
 } GameState;
+
+  GameState *game;
 
 bool tile_contains_mine(int x, int y){
   if(board[x][y]!= *mine){
@@ -32,9 +37,10 @@ bool tile_contains_mine(int x, int y){
 }
 
 void drawboard(){
+  //game->running = true;
   int i, j;
   char *p = " ";
-  printf("%s","  ABCDEFGHI\n 0");
+  printf("%s","  012345678\n 0");
   for (i=0; i< NUM_TILES_X; i++){
     for (j=0; j< NUM_TILES_Y ; j++){
       if(board[i][j]!= *mine){
@@ -42,7 +48,7 @@ void drawboard(){
       }
       printf("%c",board[i][j]);
     }
-    if(i<NUM_TILES_X){
+    if(i<NUM_TILES_Y -1){
     printf("\n %d", i+1);
   }
   }
@@ -50,7 +56,33 @@ void drawboard(){
 }
 
 void display_welcome(){
-  printf("%s","Welcome to the online Minesweeper game!\n");
+  printf("%s","Welcome to the online Minesweeper game!\n Press anykey for a board with mines\n");
+  getchar();
+}
+
+void display_menu(){
+  //game->running= false;
+  printf("%s","Sup cunt the socket connection shit will go here. For now just press any key\n\n");
+  getchar();
+}
+
+void display_game_instructions(){
+  char option_input[1], x_input[1], y_input[1];
+  printf("%s", "\nChoose an option (They aren't wokring properly yet):\n<R> Reveal tile\n<F> Place flag\n<Q>Quit\n\n Option(R,F,Q):");
+  scanf("%c",option_input);
+  if (option_input == "r"||"f"){
+    printf("%s", "\nEnter x coordinate:");
+    scanf("%c", x_input);
+    getchar();
+    printf("%s", "\nEnter y coordinate:");
+    scanf("%c", y_input);
+    getchar();
+  } else if(option_input == "q"){
+    //game->running = false;
+    display_menu();
+  } else {
+    printf("%s", "Invalid entry\n\nChoose an option:\n<R> Reveal tile\n<F> Place flag\n<Q>Quit\n\n Option(R,F,Q):");
+  }
 }
 
 void place_mines(){
@@ -68,12 +100,13 @@ void place_mines(){
 }
 
 int main(int argc, char *argv[]) {
+  //game->running = false;
   srand(RANDOM_NUMBER_SEED);
+  display_menu();
   display_welcome();
   place_mines();
   drawboard();
-
-    //Tile.revealed = false;
+  display_game_instructions();
 
 
 
