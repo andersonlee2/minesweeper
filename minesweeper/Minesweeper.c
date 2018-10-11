b294692874e4016a50c04563acd50ccaa30298fc
yugen-yeap <36917057+yugen-yeap@users.noreply.github.com>
2018-10-12 01:14:41 +1000

    Game nearly works! and other menu functions too

--- minesweeper/Minesweeper.c
+++ minesweeper/Minesweeper.c
@@ -11,13 +11,13 @@
 
 char board[NUM_TILES_X][NUM_TILES_Y], *mine = "*";
 char *revealed = "x";
-	char *flag = "+";
+char *flag = "+";
 int mine_positions[NUM_MINES][2];
-int x_input, y_input;
+int x_input, y_input, menu_option;
 
 char option_input;
   bool valid_option, game_running;
-  
+
 
 typedef struct{
   int adjacent_mines;
@@ -40,15 +40,17 @@ bool tile_contains_mine(int x, int y){
 }
 }
 
+//SAFE//
+
 void initialise_board(){
 	char *space = " ";
 	int i = 0, j = 0;
 	for (i=0; i< NUM_TILES_X; i++){
 	    for (j=0; j< NUM_TILES_Y ; j++){
-	    	board[i][j]= *space;
+	    	board[j][i]= *space;
 	    }
 	}
-	
+
 }
 
 void drawboard(){
@@ -61,7 +63,7 @@ void drawboard(){
       /*if(board[i][j]!= *mine){
         board[i][j]= *p;
       }*/
-      printf("%c",board[i][j]);
+      printf("%c",board[j][i]);
     }
     if(i<NUM_TILES_Y -1){
     printf("\n %d", i+1);
@@ -70,24 +72,8 @@ void drawboard(){
   printf("\n");
 }
 
-
-
-void display_menu(){
-  //game->running= false;
-  printf("%s","Sup cunt the socket connection shit will go here. For now just press any key\n\n");
-  getchar();
-}
-
-void display_welcome(){
-  printf("%s","Welcome to the online Minesweeper game!\n Press anykey for a board with mines\n");
-  
-  getchar();
-  game_running = true;
-}
-
 void place_mines(){
   for (int i=0; i < NUM_MINES; i++){
-
     int x,y;
     do {
       x = rand() % NUM_TILES_X;
@@ -99,49 +85,114 @@ void place_mines(){
   }
 }
 
+void display_menu(){
+  printf("%s","Sup cunt the socket connection shit will go here. For now just press any key\n");
+  getchar();
+}
+
+void display_welcome(){
+  printf("%s","Welcome to the online Minesweeper game!\n\nSelect from the following:\n<1> Play Minesweeper\n<2> Show Leaderboard\n<3> Quit\n\nChoose bitch:");
+  scanf("%d", &menu_option);
+	if (menu_option == 1){
+		initialise_board();
+		place_mines();
+	  game_running = true;
+	} else if (menu_option == 2){
+		printf("We don't have a leaderboard yet soz\n");
+
+	} else if (menu_option ==3){
+		exit(1);
+	} else {
+		printf("Enter a valid option. Select from the following:\n<1> Play Minesweeper\n<2> Show Leaderboard\n<3> Quit\n\nChoose bitch:");
+	}
+}
+
+int check_tile(int x, int y){
+	int surrounding_tiles = 0;
+	if (tile_contains_mine(x,y)){
+		surrounding_tiles = -1;
+	} else {
+		if(tile_contains_mine(x+1,y)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x-1,y)) {
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x,y-1)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x,y+1)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x+1,y+1)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x-1,y+1)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x+1,y-1)){
+			surrounding_tiles++;
+		} else if(tile_contains_mine(x-1,y-1)){
+			surrounding_tiles++;
+		}
+	}
+	return surrounding_tiles;
+}
+
 void play_game(){
-	valid_option = false;
-	
+	//place_mines();
+	!valid_option;
 	while (!valid_option){
-		
 		drawboard();
-			printf("%s", "\nChoose an option:\n<R> Reveal tile\n<F> Place flag\n <Q>Quit\n\n Option(R,F,Q):\n\n");
+			printf("%s", "\nChoose an option:\n<R> Reveal tile\n<F> Place flag\n<Q> Quit\n\n Option(R,F,Q):\n\n");
 			option_input = getchar();
 			if(option_input == 'r' || option_input == 'f' ||option_input == 'q'){
 			  valid_option = true;
 		    }
 		}
-		
 		if (option_input == 'r' || option_input == 'f'){
 			 printf("\nEnter x coordinate:");
 			 scanf("%d", &x_input);
-			 //getchar();
 			 printf("\nEnter y coordinate:");
 			 scanf("%d", &y_input);
 			 if(option_input == 'r'){
-				 board[x_input][y_input] = *revealed;
+				 int tile_no = check_tile(x_input, y_input);
+				 if (tile_no == -1){
+					 board[x_input][y_input] = *revealed;
+					 drawboard();
+					 printf("You have revealed a mine! Game over :(\n\n");
+					 game_running = false;
+					 getchar();
+					 display_welcome();
+				 }
+				 char tile_no_c = '0' + tile_no;
+				 //if (tile_no > 0){
+					 board[x_input][y_input] = tile_no_c;
+				 /*} else {
+					 while(board[x_input][y_input] == 0){
+						 check_tile(x_input-1, y_input);
+						 check_tile(x_input+1, y_input);
+						 check_tile(x_input, y_input+1);
+						 check_tile(x_input, y_input-1);
+						 check_tile(x_input-1, y_input-1);
+						 check_tile(x_input+1, y_input+1);
+						 check_tile(x_input-1, y_input+1);
+						 check_tile(x_input+1, y_input-1);
+					 }
+				 }*/
 			 } else {
 				 board[x_input][y_input] = *flag;
 			 }
-			 drawboard();
+			 //drawboard();
 			 valid_option = false; //return to options
-		} else if(option_input == 'q'){	
+		} else if(option_input == 'q'){
 			 game_running = false;
 			 display_welcome();
 		}
 }
 
 int main(int argc, char *argv[]) {
-	initialise_board();
+	//initialise_board();
 	srand(RANDOM_NUMBER_SEED);
 	display_menu();
 	display_welcome();
-	place_mines();
 	//drawboard();
 	while(game_running){
-		play_game();	
+		play_game();
 	}
-	
-	
   return 0;
 }
